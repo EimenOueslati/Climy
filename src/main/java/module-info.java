c7df54dddd44224d3706eeda79cc07b508fdb13c
Eimen Ouesalti <aymanoueslati22@gmail.com>
2022-12-19 08:43:35 +0100

    Updated the pom file with the necessary dependencies and added some plugins.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module foodie.gj.foodie {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens foodie.gj.foodie to javafx.fxml;
     exports foodie.gj.foodie;
 }
